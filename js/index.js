6598ad91d70d4e7d13c1e55c9471183f10ede263
sunzhuo <sunyumei_rd@ayibang.com>
2022-02-11 18:05:50 +0800

    feat: 枚举测试

--- js/index.js
+++ js/index.js
@@ -5,3 +5,13 @@ function greeter(person) {
 let user = 'SUN ZHUO';
 const tsDiv = document.getElementById('tsDiv'); //感叹号(非空断言操作符) ，如果变量可能为空，可以用这个，如果为空，会丢出断言失败。
 tsDiv.innerHTML = greeter(user);
+//枚举示例
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+let c = Color.Green;
+console.log(c);
+console.log(Color);
--- ts/index.ts
+++ ts/index.ts
@@ -4,3 +4,8 @@ function greeter(person: string) {
 let user = 'SUN ZHUO';
 const tsDiv:HTMLElement = document.getElementById('tsDiv')!; //感叹号(非空断言操作符) ，如果变量可能为空，可以用这个，如果为空，会丢出断言失败。
 tsDiv.innerHTML = greeter(user);
+//枚举示例
+enum Color {Red, Green, Blue}
+let c: Color = Color.Green;
+console.log(c);
+console.log(Color);
