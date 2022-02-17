7c919b3b87c369704c773bdb54bcdd4380c954ec
sunzhuo <sunyumei_rd@ayibang.com>
2022-02-17 15:33:18 +0800

    fix: 添加测试例子

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 function greeter(person) {
     return 'Hello, ' + person;
 }
+let isDone = false;
 let user = 'SUN ZHUO';
 const tsDiv = document.getElementById('tsDiv'); //感叹号(非空断言操作符) ，如果变量可能为空，可以用这个，如果为空，会丢出断言失败。
 tsDiv.innerHTML = greeter(user);
@@ -13,5 +14,13 @@ var Color;
     Color[Color["Blue"] = 2] = "Blue";
 })(Color || (Color = {}));
 let c = Color.Green;
+let colorName = Color[2];
+console.log(colorName); // 显示'Green'因为上面代码里它的值是2
 console.log(c);
 console.log(Color);
+let notSure = 4;
+// notSure.ifItExists(); // okay, ifItExists might exist at runtime
+notSure.toFixed(); // okay, toFixed exists (but the compiler doesn't check)
+console.log(notSure.toFixed());
+create({ prop: 0 }); // OK
+create(null); // OK
--- ts/index.ts
+++ ts/index.ts
@@ -1,11 +1,27 @@
 function greeter(person: string) {
     return 'Hello, ' + person;
 }
+let isDone: boolean = false;
 let user = 'SUN ZHUO';
 const tsDiv:HTMLElement = document.getElementById('tsDiv')!; //感叹号(非空断言操作符) ，如果变量可能为空，可以用这个，如果为空，会丢出断言失败。
 tsDiv.innerHTML = greeter(user);
 //枚举示例
 enum Color {Red, Green, Blue}
 let c: Color = Color.Green;
+let colorName: string = Color[2];
+console.log(colorName);  // 显示'Green'因为上面代码里它的值是2
 console.log(c);
 console.log(Color);
+
+let notSure: any = 4;
+// notSure.ifItExists(); // okay, ifItExists might exist at runtime
+notSure.toFixed(); // okay, toFixed exists (but the compiler doesn't check)
+console.log(notSure.toFixed());
+// let prettySure: Object = 4;
+// prettySure.toFixed(); // Error: Property 'toFixed' doesn't exist on type 'Object'.
+
+declare function create(o: object | null): void;
+
+create({ prop: 0 }); // OK
+create(null); // OK
+
